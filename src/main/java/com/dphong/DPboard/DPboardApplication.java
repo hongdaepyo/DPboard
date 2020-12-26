e41230e00e18132fc0d28ac2cd2117db6570059f
Hong Daepyo <h1o1n1g1@nate.com>
2020-12-26 18:17:57 +0900

    first commit

---
+++ src/main/java/com/dphong/DPboard/DPboardApplication.java
@@ -0,0 +1,13 @@
+package com.dphong.DPboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DPboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DPboardApplication.class, args);
+	}
+
+}
